d9b8721f7c8809c3eb6c5fff98180f6e39f2c8f6
JJTofflemire <andyababcock@gmail.com>
2024-03-18 00:08:29 -0700

    feat: remove html snippets from rust script

--- cookbook/src/main.rs
+++ cookbook/src/main.rs
@@ -3,57 +3,6 @@ use std::fs;
 use std::io::{self};
 
 fn main() -> io::Result<()> {
-    let index_header_html = r#"
-    <!DOCTYPE html>
-    <html lang="en">
-    
-    <head>
-        <meta charset="UTF-8">
-        <title>jtof.dev</title>
-        <link rel="stylesheet" href="/styles.css">
-        <link rel="stylesheet" href="notes.css">
-        <link rel="icon" href="/assets/ferris the crab.png">
-        <script src="/script.js"></script>
-    </head>
-    
-    <body class="dark-mode">
-        <div class="switch-and-notes">
-        <div class="left-actions">
-        <a href="/">
-            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="-5.0 -10.0 110.0 120.0"
-                class="homepage-svg">
-                <path
-                    d="m87.5 44.844c-0.15625-1.0938-0.78125-2.0312-1.5625-2.6562l-9.6875-7.1875v-14.844c0-2.3438-1.875-4.0625-4.0625-4.0625h-5.9375c-2.3438 0-4.0625 1.875-4.0625 4.0625v4.375l-10.938-8.125c-0.625-0.46875-1.25-0.46875-1.875 0l-35.156 25.625c-0.9375 0.625-1.4062 1.5625-1.5625 2.6562s0.15625 2.1875 0.78125 3.125l3.9062 5.3125c1.4062 1.875 3.9062 2.1875 5.7812 0.78125l0.78125-0.625v26.562c0 2.3438 1.875 4.0625 4.0625 4.0625h44.062c2.3438 0 4.0625-1.875 4.0625-4.0625v-26.562l0.78125 0.625c0.78125 0.625 1.5625 0.78125 2.5 0.78125 1.25 0 2.5-0.625 3.2812-1.7188l3.9062-5.3125c0.78125-0.625 1.0938-1.7188 0.9375-2.8125zm-22.5-24.844c0-0.625 0.46875-0.9375 0.9375-0.9375h5.9375c0.625 0 0.9375 0.46875 0.9375 0.9375v12.656l-7.8125-6.0938zm-6.875 61.094h-16.25v-21.406h16.406v21.406zm14.844-1.0938c0 0.625-0.46875 0.9375-0.9375 0.9375h-10.625v-22.812c0-0.9375-0.625-1.5625-1.5625-1.5625h-19.531c-0.9375 0-1.5625 0.625-1.5625 1.5625v22.969h-10.625c-0.625 0-0.9375-0.46875-0.9375-0.9375v-29.062l22.812-17.031 22.969 17.031zm11.25-34.062-3.9062 5.3125c-0.3125 0.46875-0.9375 0.46875-1.4062 0.15625l-27.969-20.625c-0.3125-0.15625-0.625-0.3125-0.9375-0.3125s-0.625 0.15625-0.9375 0.3125l-27.812 20.781c-0.46875 0.3125-1.0938 0.3125-1.4062-0.15625l-3.9062-5.3125c-0.3125-0.3125-0.3125-0.625-0.3125-0.78125 0-0.3125 0.15625-0.46875 0.46875-0.625l33.906-25.312 33.906 25.312c0.15625 0.15625 0.3125 0.46875 0.46875 0.625 0 0.15625 0 0.46875-0.15625 0.625z" />
-                <title>go back to the homepage</title>
-            </svg>
-        </a>
-        <label class="switch">
-            <input type="checkbox" checked id="checkbox" onchange="switchcolors(event)">
-            <span class="slider round"></span>
-        </label>
-    </div>
-            <h2 class="recipe-name">cookbook search</h2>
-            <div style="width: 60px"></div>
-        </div>
-        <input type="text" onkeyup="search_recipes(event)" placeholder="search recipes" class="search-bar">
-        <div id="recipe-links">
-"#;
-
-    let footer_html = r#"
-<footer>
-<br>
-<br>
-<hr>
-<p class="footer">this <a href="https://github.com/jtof-dev/jtof.dev" target="_blank">website</a> is
-    open-source, and is licensed under <a href="https://www.gnu.org/licenses/gpl-3.0.html"
-        target="_blank">gpl-3</a><br>powered by <a href="https://pages.github.com" target="_blank">github
-        pages</a><br>developed by <a href="https://github.com/jtof-dev" target="_blank">jtof</a></p>
-</footer>
-</body>
-
-</html>
-"#;
-
     // Specify the directory path
     let directory_path = "../../../obsidian vault/recipes/";
     // let directory_path = "recipes/";
@@ -84,7 +33,6 @@ fn main() -> io::Result<()> {
                         entry_name,
                         entry_name_with_spaces,
                         entry_contents,
-                        footer_html,
                     );
                 }
                 Err(err) => {
@@ -93,7 +41,7 @@ fn main() -> io::Result<()> {
             }
         }
     }
-    make_index_page(index_header_html, footer_html);
+    make_index_page();
     Ok(())
 }
 
@@ -101,8 +49,9 @@ fn make_recipe_pages(
     markdown_name: String,
     markdown_name_with_spaces: String,
     mut markdown_contents: String,
-    footer_html: &str,
 ) {
+    let footer_html = fs::read_to_string("src/footer_html.html").unwrap();
+    let first_header = fs::read_to_string("src/first_header.html").unwrap();
     let start_index = markdown_contents.find("---").unwrap_or(0);
     let end_index = markdown_contents
         .rfind("---")
@@ -126,36 +75,6 @@ fn make_recipe_pages(
         format!("<span class=\"notes\">{}</span>", matched_text)
     });
 
-    let first_header = r#"
-    <!DOCTYPE html>
-    <html lang="en">
-    
-    <head>
-        <meta charset="UTF-8">
-        <title>jtof.dev</title>
-        <link rel="stylesheet" href="/styles.css">
-        <link rel="stylesheet" href="/cookbook/notes.css">
-        <link rel="icon" href="/assets/ferris the crab.png">
-        <script src="/script.js"></script>
-    </head>
-    
-    <body class="dark-mode">
-        <div class="switch-and-notes">
-            <div class="left-actions">
-                <a href="/cookbook/">
-                    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="-5.0 -10.0 110.0 120.0"
-                        class="homepage-svg">
-                        <path
-                            d="m87.5 44.844c-0.15625-1.0938-0.78125-2.0312-1.5625-2.6562l-9.6875-7.1875v-14.844c0-2.3438-1.875-4.0625-4.0625-4.0625h-5.9375c-2.3438 0-4.0625 1.875-4.0625 4.0625v4.375l-10.938-8.125c-0.625-0.46875-1.25-0.46875-1.875 0l-35.156 25.625c-0.9375 0.625-1.4062 1.5625-1.5625 2.6562s0.15625 2.1875 0.78125 3.125l3.9062 5.3125c1.4062 1.875 3.9062 2.1875 5.7812 0.78125l0.78125-0.625v26.562c0 2.3438 1.875 4.0625 4.0625 4.0625h44.062c2.3438 0 4.0625-1.875 4.0625-4.0625v-26.562l0.78125 0.625c0.78125 0.625 1.5625 0.78125 2.5 0.78125 1.25 0 2.5-0.625 3.2812-1.7188l3.9062-5.3125c0.78125-0.625 1.0938-1.7188 0.9375-2.8125zm-22.5-24.844c0-0.625 0.46875-0.9375 0.9375-0.9375h5.9375c0.625 0 0.9375 0.46875 0.9375 0.9375v12.656l-7.8125-6.0938zm-6.875 61.094h-16.25v-21.406h16.406v21.406zm14.844-1.0938c0 0.625-0.46875 0.9375-0.9375 0.9375h-10.625v-22.812c0-0.9375-0.625-1.5625-1.5625-1.5625h-19.531c-0.9375 0-1.5625 0.625-1.5625 1.5625v22.969h-10.625c-0.625 0-0.9375-0.46875-0.9375-0.9375v-29.062l22.812-17.031 22.969 17.031zm11.25-34.062-3.9062 5.3125c-0.3125 0.46875-0.9375 0.46875-1.4062 0.15625l-27.969-20.625c-0.3125-0.15625-0.625-0.3125-0.9375-0.3125s-0.625 0.15625-0.9375 0.3125l-27.812 20.781c-0.46875 0.3125-1.0938 0.3125-1.4062-0.15625l-3.9062-5.3125c-0.3125-0.3125-0.3125-0.625-0.3125-0.78125 0-0.3125 0.15625-0.46875 0.46875-0.625l33.906-25.312 33.906 25.312c0.15625 0.15625 0.3125 0.46875 0.46875 0.625 0 0.15625 0 0.46875-0.15625 0.625z" />
-                        <title>go back to the cookbook search page</title>
-                    </svg>
-                </a>
-                <label class="switch">
-                    <input type="checkbox" checked id="checkbox" onchange="switchcolors(event)">
-                    <span class="slider round"></span>
-                </label>
-            </div>
-    "#;
     let second_header = format!(
         "<h2 class=\"recipe-name\">{}</h2>        
         <button onclick=\"show_hide_notes()\" id=\"notes-button\">hide my notes</button>
@@ -167,7 +86,7 @@ fn make_recipe_pages(
         "{}{}{}{}",
         first_header, second_header, modified_html_contents, footer_html
     );
-    if let Ok(dir) = fs::read_dir(&markdown_name) {
+    if let Ok(_) = fs::read_dir(&markdown_name) {
         fs::write(
             format!("{}/index.html", &markdown_name),
             final_html_contents.as_bytes(),
@@ -183,7 +102,10 @@ fn make_recipe_pages(
     }
 }
 
-fn make_index_page(index_header_html: &str, footer_html: &str) {
+fn make_index_page() {
+
+    let index_header_html = fs::read_to_string("src/index_header_html.html").unwrap();
+    let footer_html = fs::read_to_string("src/footer_html.html").unwrap();
 
     let mut list_of_recipes = String::new();
 
@@ -200,7 +122,6 @@ fn make_index_page(index_header_html: &str, footer_html: &str) {
         // Check if the entry is a file
         if entry.file_type().unwrap().is_file() {
             // Get the file path
-            let file_path = entry.path();
             let entry_name_full = entry.file_name().into_string().unwrap();
             let entry_name_with_spaces: String = entry_name_full
                 .chars()
