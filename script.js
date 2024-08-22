128d3b76b2697a2a175d8925a7a80b1e7e42047c
Andy Babcock <contact@jtof.dev>
2024-08-22 13:52:58 -0700

    fix: remove autoformatting from template html files

--- script.js
+++ script.js
@@ -1,71 +1,77 @@
 function initial_setup() {
-    if (window.localStorage.getItem("dark") === null) {
-        window.localStorage.setItem("dark", true)
-    }
-    if (window.localStorage.getItem("dark") === "true") {
-        document.body.className = "dark-mode"
-        document.getElementById("checkbox").checked = 1
-    }
-    else {
-        document.body.className = "light-mode"
-        document.getElementById("checkbox").checked = 0
-    }
-    if (window.localStorage.getItem("notes") === null) {
-        window.localStorage.setItem("notes", true)
-    }
-    if (window.localStorage.getItem("notes") === "true") {
-        document.querySelectorAll("span.notes").forEach(Element => Element.className = "notes")
-        document.getElementById("notes-button").innerHTML = "hide my notes"
-    }
-    else {
-        document.querySelectorAll("span.notes").forEach(Element => Element.className = "notes hide-notes")
-        document.getElementById("notes-button").innerHTML = "show my notes"
-    }
+  if (window.localStorage.getItem("dark") === null) {
+    window.localStorage.setItem("dark", true);
+  }
+  if (window.localStorage.getItem("dark") === "true") {
+    document.body.className = "dark-mode";
+    document.getElementById("checkbox").checked = 1;
+  } else {
+    document.body.className = "light-mode";
+    document.getElementById("checkbox").checked = 0;
+  }
+  if (window.localStorage.getItem("notes") === null) {
+    window.localStorage.setItem("notes", true);
+  }
+  if (window.localStorage.getItem("notes") === "true") {
+    document
+      .querySelectorAll("span.notes")
+      .forEach((Element) => (Element.className = "notes"));
+    document.getElementById("notes-button").innerHTML = "hide my notes";
+  } else {
+    document
+      .querySelectorAll("span.notes")
+      .forEach((Element) => (Element.className = "notes hide-notes"));
+    document.getElementById("notes-button").innerHTML = "show my notes";
+  }
 }
 
 function switchcolors(event) {
-    if (event.target.checked) { // if true
-        document.body.className = "dark-mode"
-        window.localStorage.setItem("dark", true)
-    }
-    else { // if false
-        document.body.className = "light-mode"
-        window.localStorage.setItem("dark", false)
-    }
+  if (event.target.checked) {
+    // if true
+    document.body.className = "dark-mode";
+    window.localStorage.setItem("dark", true);
+  } else {
+    // if false
+    document.body.className = "light-mode";
+    window.localStorage.setItem("dark", false);
+  }
 }
 
 function show_hide_notes() {
-    let notes = window.localStorage.getItem("notes")
-    window.localStorage.setItem("notes", notes === "true" ?"false":"true")
-    let updated_notes = notes === "true" ?"false":"true"
-    if (updated_notes === "true") {
-        document.querySelectorAll("span.notes").forEach(Element => Element.className = "notes")
-        document.getElementById("notes-button").innerHTML = "hide my notes"
-    }
-    else {
-        document.querySelectorAll("span.notes").forEach(Element => Element.className = "notes hide-notes")
-        document.getElementById("notes-button").innerHTML = "show my notes"
-    }
+  let notes = window.localStorage.getItem("notes");
+  window.localStorage.setItem("notes", notes === "true" ? "false" : "true");
+  let updated_notes = notes === "true" ? "false" : "true";
+  if (updated_notes === "true") {
+    document
+      .querySelectorAll("span.notes")
+      .forEach((Element) => (Element.className = "notes"));
+    document.getElementById("notes-button").innerHTML = "hide my notes";
+  } else {
+    document
+      .querySelectorAll("span.notes")
+      .forEach((Element) => (Element.className = "notes hide-notes"));
+    document.getElementById("notes-button").innerHTML = "show my notes";
+  }
 }
 
 function search_recipes(event) {
-    let links = Array.from(document.querySelectorAll('a.recipe-link'))
-    links.filter(link => !link.innerHTML.includes(event.target.value)).forEach(Element => Element.className = "recipe-link hide-recipe")
-    links.filter(link => link.innerHTML.includes(event.target.value)).forEach(Element => Element.classList.remove("hide-recipe"))
+  let links = Array.from(document.querySelectorAll("a.recipe-link"));
+  links
+    .filter((link) => !link.innerHTML.includes(event.target.value))
+    .forEach((Element) => (Element.className = "recipe-link hide-recipe"));
+  links
+    .filter((link) => link.innerHTML.includes(event.target.value))
+    .forEach((Element) => Element.classList.remove("hide-recipe"));
 }
 
-document.addEventListener("DOMContentLoaded", initial_setup)
+document.addEventListener("DOMContentLoaded", initial_setup);
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Extract links from HTML and store them in an array
-    let links = Array.from(document.querySelectorAll('a.recipe-link'));
+document.addEventListener("DOMContentLoaded", function () {
+  let links = Array.from(document.querySelectorAll("a.recipe-link"));
 
-    // Sort the array alphabetically based on the text content of the links
-    links.sort((a, b) => a.textContent.localeCompare(b.textContent));
+  links.sort((a, b) => a.textContent.localeCompare(b.textContent));
 
-    // Create a new HTML string with the sorted links and line breaks
-    let sortedHTML = links.map(link => link.outerHTML).join('');
+  let sortedHTML = links.map((link) => link.outerHTML).join("");
 
-    // Replace the existing HTML with the sorted HTML
-    document.getElementById('recipe-links').innerHTML = sortedHTML;
+  document.getElementById("recipe-links").innerHTML = sortedHTML;
 });
